19f9af009d3316ac8c17eff8addd2ee476980b6a
horlumiday <oluwadareysamuel@yahoo.com>
2019-05-29 17:33:24 +0100

    added third party plugin(ekko lightbox) on image gallery

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-$(function() {
-  // Auto play modal video
-  $(".video").click(function () {
-    var theModal = $(this).data("target"),
-    videoSRC = $(this).attr("data-video"),
-    videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
-    $(theModal + ' iframe').attr('src', videoSRCauto);
-    $(theModal + ' button.close').click(function () {
-      $(theModal + ' iframe').attr('src', videoSRC);
-    });
-  });
+$(function () {
+	// Auto play modal video
+	$(".video").click(function () {
+		var theModal = $(this).data("target"),
+			videoSRC = $(this).attr("data-video"),
+			videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+		$(theModal + ' iframe').attr('src', videoSRCauto);
+		$(theModal + ' button.close').click(function () {
+			$(theModal + ' iframe').attr('src', videoSRC);
+		});
+	});
 });
+
+
+$(document).ready(function() {
+	
+	 $('a[data-toggle="lightbox"]').click(function(e){
+		 e.preventDefault();
+		 
+		 $(this).ekkoLightbox();
+	 })
+
+})
